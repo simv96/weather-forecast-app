6a915ed4a7b4c7e78530d2a7f884f3313f373e8b
simv96 <17virdees@gmail.com>
2024-01-31 21:34:50 +0000

    Add in JS for city search

--- src/index.js
+++ src/index.js
@@ -27,3 +27,22 @@ function formatDate(date) {
 let currentDateELement = document.querySelector("#date-time");
 let currentDate = new Date();
 currentDateELement.innerHTML = formatDate(currentDate);
+
+function searchCity(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-city");
+  let cityHeading = document.querySelector("#city-header");
+  cityHeading.innerHTML = `${city.value}`;
+
+  function showCurrentWeather(response) {
+    let temp = Math.round(response.data.temperature.current);
+    let tempHeader = document.querySelector("#current-temp-value");
+    tempHeader.innerHTML = `${temp}`;
+  }
+  let cityValue = city.value;
+  let apiKey = "ee21f04bf3f2ad6e420ef9to7c4ad1a4";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityValue}&key=${apiKey}`;
+  axios.get(apiUrl).then(showCurrentWeather);
+}
+let cityInput = document.querySelector("#search-bar");
+cityInput.addEventListener("submit", searchCity);
