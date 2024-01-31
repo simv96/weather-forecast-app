9cea949753d03922e8ad86e6a2ad8d42667cf1b4
simv96 <17virdees@gmail.com>
2024-01-31 13:08:49 +0000

    Add JS for updating the date and time

---
+++ src/index.js
@@ -0,0 +1,29 @@
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
+let currentDateELement = document.querySelector("#date-time");
+let currentDate = new Date();
+currentDateELement.innerHTML = formatDate(currentDate);
